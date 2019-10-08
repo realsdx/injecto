7a93a6ac9f72466a2e77882bbe67ba397765575a
realsdx <realsdx@protonmail.com>
2019-10-08 15:06:03 +0530

    improve output

--- injecto.c
+++ injecto.c
@@ -12,14 +12,15 @@
 
 void print_regs(struct user_regs_struct regs)
 {
-    printf("\033[1;32m[+]rip:\033[1;m%p\n", (void *)regs.rip);
-    printf("\033[1;32m[+]rsp:\033[1;m%p\n", (void *)regs.rsp);
-    printf("\033[1;32m[+]rbp:\033[1;m%p\n", (void *)regs.rbp);
-    printf("\033[1;32m[+]rsi:\033[1;m%p\n", (void *)regs.rsi);
-    printf("\033[1;32m[+]rdi:\033[1;m%p\n", (void *)regs.rdi);
-    printf("\033[1;32m[+]rax:\033[1;m%p\n", (void *)regs.rax);
-    printf("\033[1;32m[+]rbx:\033[1;m%p\n", (void *)regs.rbx);
-    printf("\033[1;32m[+]rcx:\033[1;m%p\n", (void *)regs.rcx);
+	printf("\n\033[1;33mResgister Info:\033[1;m \n");
+    printf("\033[1;32mrip:\033[1;m%p\n", (void *)regs.rip);
+    printf("\033[1;32mrsp:\033[1;m%p\n", (void *)regs.rsp);
+    printf("\033[1;32mrbp:\033[1;m%p\n", (void *)regs.rbp);
+    printf("\033[1;32mrsi:\033[1;m%p\n", (void *)regs.rsi);
+    printf("\033[1;32mrdi:\033[1;m%p\n", (void *)regs.rdi);
+    printf("\033[1;32mrax:\033[1;m%p\n", (void *)regs.rax);
+    printf("\033[1;32mrbx:\033[1;m%p\n", (void *)regs.rbx);
+    printf("\033[1;32mrcx:\033[1;m%p\n", (void *)regs.rcx);
 }
 
 int main(int argc, char *argv[])
@@ -35,15 +36,16 @@ int main(int argc, char *argv[])
     }
 
     target = atoi(argv[1]);
-    printf("+ Target process id: %d \n",target);
+    printf("\033[1;33m[!]\033[1;m Target process id: %d \n",target);
     
     if(ptrace(PTRACE_ATTACH, target, NULL, NULL) < 0){
         perror("Error: ATTACH");
         exit(1);
     }
-    printf("+ Waiting for process's SIGTRAP ...\n ");
+    printf("\033[1;33m[!]\033[1;m Waiting for process's SIGTRAP...\n");
     wait(NULL);
-    printf("+ Getting registers of the process...\n ");
+
+    printf("\033[1;32m[+]\033[1;m Getting registers of the process...\n");
     if(ptrace(PTRACE_GETREGS, target, NULL, &regs) < 0){
         perror("Error: GETREGS");
         exit(1);
